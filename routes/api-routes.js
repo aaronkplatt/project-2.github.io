805cf90d09e90d051ee2b4b1b8a19f604189e9fd
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2020-10-06 18:22:06 -0700

    Added wireframe for api-routes.js.

---
+++ routes/api-routes.js
@@ -0,0 +1,5 @@
+// We want to use this file to:
+// GET view high scores from db for any assortment of games
+// POST high scores to our website
+// PUT update high scores if the user beats their previous score 
+// DELETE high scores if the user doesn't want to keep it 
