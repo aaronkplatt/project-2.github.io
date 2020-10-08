d052580d06ac54f79bf61ddfe719f6bb91871b5c
Monk <tbsanders5@gmail.com>
2020-10-08 13:03:31 -0700

    working on html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = function(app) {
+
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../"))
+    });
+
+    app.get("/index.handlebars", function(req, res) {
+        res.sendFile(path.join(__dirname, "../"))
+    });
+
+    app.get("/game", function(req, res) {
+        res.sendFile(path.join(__dirname, "../views"))
+    });
+
+   
+} 
