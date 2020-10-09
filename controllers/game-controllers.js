ffad8b0125c15eab62e6a3093ac49d10a94591d5
Monk <tbsanders5@gmail.com>
2020-10-08 20:03:59 -0700

    submit rerouting

--- controllers/game-controllers.js
+++ controllers/game-controllers.js
@@ -1,14 +1,14 @@
-var express = require("express");
+// var express = require("express");
 
-var router = express.Router();
+// var router = express.Router();
 
-router.get("/", function(req, res) {
-      res.render("index");
-});
+// router.get("/", function(req, res) {
+//       res.render("index");
+// });
 
-router.get("/games", function(req, res) {
-      res.render("game");
-});
+// router.get("/games", function(req, res) {
+//       res.render("game");
+// });
 
 
-module.exports = router;
+// module.exports = router;
