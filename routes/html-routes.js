015ff8992ea9eacead201dd7d69bd7932914d018
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2020-10-10 10:58:13 -0700

    Added html routes for games.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,7 +13,6 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../views/game.handlebars"));
     });
 
-
     // Route for getting username to be used in game.handlebars
     app.get("/api/users", function (req, res) {
         console.log("req.parmas: \n", req.params);
@@ -28,12 +27,6 @@ module.exports = function (app) {
         });
     });
 
-    // Route for logging user out
-    // app.get("/logout", function (req, res) {
-    //     req.logout();
-    //     res.redirect("/");
-    // });
-
     // Route for posting new user data to be used in database
     app.post("/games", async function (req, res) {
         console.log("POST request req.body \n", req.body);
@@ -47,12 +40,24 @@ module.exports = function (app) {
                 // res.json(dbUserData); //shows new data in browser
                 res.render("game");
                 return dbUserData.dataValues;
-                
+
             })
             .catch(function (error) {
                 console.log("Inside of catch from userinfo POST: \n", error);
                 // res.status(401).json(error);
             });
         console.log("This is the User Obj: \n", userObj);
+    });
+
+    app.get("/games/snake", function (req, res) {
+        res.sendFile(path.join(__dirname, "../games/snake/snake.html"));
+    });
+
+    app.get("/games/Flappy_Bird", function (req, res) {
+        res.sendFile(path.join(__dirname, "../games/Flappy_Bird/index.html"));
+    });
+
+    app.get("/games/Ping_Pong", function (req, res) {
+        res.sendFile(path.join(__dirname, "../games/Ping_Pong/index.html"));
     });
 } 
