501ae2227cae4821192c70b69bdbb109d0237c35
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2020-10-13 20:19:19 -0700

    Session score submission fully operational

--- routes/score-api-routes.js
+++ routes/score-api-routes.js
@@ -1,30 +1,35 @@
 const db = require("../models");
-
-module.exports = function (app) {
-    app.get("/api/scores", function(req, res) {
-
-        db.Score.findAll({
-        }).then(function(dbScore) {
-            res.json(dbScore);
-        });
+module.exports = function(app) {
+  app.get("/api/scores", function(req, res) {
+    db.Score.findAll({}).then(function(dbScore) {
+      res.json(dbScore);
     });
-
-    app.post("/api/scores", function (req, res) {
-        db.Score.create(req.body).then(function(dbScore) {
-            res.json(dbScore);
-        });
+  });
+  //   app.post("/api/scores", function(req, res) {
+  //     let data = {
+  //       username: req.session.username,
+  //     //   game_name: req.body.game_anme,
+  //       score: req.body.score
+  //     };
+  //     db.Snake.create(data).then(function(dbScore) {
+  //       res.json(dbScore);
+  //     });
+  //   });
+  app.post("/api/snake", function(req, res) {
+    let score_info = { score: req.body.sessionScore, UserId: req.body.userId };
+    console.log("Score POST request req.body \n", score_info);
+    db.Snake.create(score_info).then(function(res, err) {
+      if (err) throw err;
+      console.log("display score_info: \n", score_info);
+    }); ///pass the score to db
+  });
+  app.delete("/api/score:scores_id", function(req, res) {
+    db.Score.destroy({
+      where: {
+        id: req.params.scores_id
+      }
+    }).then(function(dbScore) {
+      res.json(dbScore);
     });
-
-    app.delete("/api/score:scores_id", function (req, res) {
-        db.Score.destroy({
-            where: {
-                id: req.params.scores_id
-            }
-        }).then(function(dbScore) {
-            res.json(dbScore);
-        });
-    });
-
-
+  });
 }
-
--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,63 +1,68 @@
 // **************************************************************
 // We want to use this file to:
-
 // Offer a set of routes for displaying and saving data to the db
 // GET view high scores from db for any assortment of games
 // POST high scores to our website
 // PUT update high scores if the user beats their previous score 
 // DELETE high scores if the user doesn't want to keep it 
 // **************************************************************
-
 // Dependencies
 // ===============================================================
-
 // Requirering our models
+// const { json } = require("sequelize/types");
+// const { Json } = require("sequelize/types/lib/utils");
 var db = require("../models");
-
 // Routes 
 // ===============================================================
-module.exports = function (app) {
-
-    // GET route for getting all users, with highscores for every game they've played
-    app.get("/api/users", function (request, response) {
-        db.User.findAll({}).then(function (dbUser) {
-            response.json(dbUser);
-        });
+module.exports = function(app) {
+  // GET route for getting all users, with highscores for every game they've played
+  app.get("/api/users", function(request, response) {
+    db.User.findAll({}).then(function(dbUser) {
+      response.json(dbUser);
     });
-
-    // GET route for getting all high scores, from every game, associated with a specific user
-    app.get("/api/users/:users_id", function (request, response) {
-        db.User.findOne({
-            where: {
-                id: request.params.id
-            },
-            include: [db.Score]
-        }).then(function (dbUser) {
-            response.json(dbUser);
-        });
+  });
+  app.get("/api/users/sessionID", function(request, response) {
+    console.log("/api/users/sessionID called");
+    let id;
+    // console.log(`db keys ${Object.keys(db)}`);
+    db.User.findAll({}).then(function(dbUser) {
+      const table = JSON.parse(JSON.stringify(dbUser));
+      const current_username = request.session.username;
+      table.forEach(user => {
+        console.log(`compare ${user.name},${current_username}`);
+        if (user.name === current_username) id = user.id;
+      });
+      console.log(`id: ${id}`);
+      response.json(id);
     });
-
-    // GET route for getting all comments a user has made for a specific game?
-
-    // GET route for getting all comments a user has made for all games they've completed?
-
-    // POST route for adding a new user
-    app.post("/api/users", function (request, response) {
-        db.User.create(request.body).then(function (dbUser) {
-            response.json(dbUser);
-        })
+  });
+  // GET route for getting all high scores, from every game, associated with a specific user
+  app.get("/api/users/:users_id", function(request, response) {
+    db.User.findOne({
+      where: {
+        id: request.params.id
+      },
+      include: [db.Score]
+    }).then(function(dbUser) {
+      response.json(dbUser);
     });
-
-
-
-    // DELETE route to remove an existing user
-    app.delete("/api/users/:users_id", function (request, response) {
-        db.User.destroy({
-            where: {
-                id: request.params.id
-            }
-        }).then(function (dbUser) {
-            response.json(dbUser);
-        });
+  });
+  // GET route for getting all comments a user has made for a specific game?
+  // GET route for getting all comments a user has made for all games they've completed?
+  // POST route for adding a new user
+  app.post("/api/users", function(request, response) {
+    db.User.create(request.body).then(function(dbUser) {
+      response.json(dbUser);
+    })
+  });
+  // DELETE route to remove an existing user
+  app.delete("/api/users/:users_id", function(request, response) {
+    db.User.destroy({
+      where: {
+        id: request.params.id
+      }
+    }).then(function(dbUser) {
+      response.json(dbUser);
     });
+  });
 };
