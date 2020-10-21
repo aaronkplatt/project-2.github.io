3be7150477d3e4bf67ff741fa2ef776347fe8a29
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2020-10-20 17:17:03 -0700

    replaced await with .then()

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -17,42 +17,36 @@ module.exports = function(app, express) {
       res.json(dbUser);
     });
   });
-  app.post("/createUser", async function(req, res) { //sign up form submission
+  app.post("/createUser", function(req, res) { //sign up form submission
     let userSubmission = {
       name: req.body.userName,
       password: req.body.password
     };
-    // console.log(req.body);
-    // console.log(userSubmission);
     let errmsg = "";
-    //verifying username
-    if (userSubmission.name === undefined || userSubmission.name === null) errmsg += "Username is null. ";
-    else if (userSubmission.name.indexOf(" ") != -1) errmsg += "Username cannot contain spaces. ";
-    else if (userSubmission.name.length > 46) errmsg += "Username too large. Must be less than 45 characters. ";
-    else if (userSubmission.name.length < 1) errmsg += "Username too small. Must be at least 1 character. ";
-    //password verification
-    if (userSubmission.password === undefined || userSubmission.password === null) errmsg = "Password field is null. ";
-    else if (userSubmission.password.indexOf(" ") != -1) errmsg += "Password cannot contain spaces.";
-    else if (userSubmission.password.length > 46) errmsg += "Password too large. Must be less than 45 characters. ";
-    else if (userSubmission.password.length < 1) errmsg += "Password too small. Must be at least 1 character. ";
-    //for testing, uncomment 'success' and comment out the create block
-    // undo the above actions when you commit
-    //delete these comments when we finalize the code.
-    // return res.json("success");
-    await db.User.findAll({ where: { name: userSubmission.name } }).then(function(dataRaw) {
+    db.User.findAll({ where: { name: userSubmission.name } }).then(function(dataRaw) {
+      if (userSubmission.name === undefined || userSubmission.name === null) errmsg += "Username is null. ";
+      else if (userSubmission.name.indexOf(" ") != -1) errmsg += "Username cannot contain spaces. ";
+      else if (userSubmission.name.length > 46) errmsg += "Username too large. Must be less than 45 characters. ";
+      else if (userSubmission.name.length < 1) errmsg += "Username too small. Must be at least 1 character. ";
+      //password verification
+      if (userSubmission.password === undefined || userSubmission.password === null) errmsg = "Password field is null. ";
+      else if (userSubmission.password.indexOf(" ") != -1) errmsg += "Password cannot contain spaces.";
+      else if (userSubmission.password.length > 46) errmsg += "Password too large. Must be less than 45 characters. ";
+      else if (userSubmission.password.length < 1) errmsg += "Password too small. Must be at least 1 character. ";
+      //verifying username
       if (dataRaw.length >= 1) errmsg += "Username must be unique. ";
+    }).then(() => {
+      if (errmsg.length > 0) {
+        res.json(errmsg);
+      } else {
+        db.User.create(userSubmission).then(function(dbUserData) {
+          req.session.username = userSubmission.name;
+          res.json("/games");
+        }).catch(function(error) {
+          console.log("Inside of catch from /createUser POST: \n", error);
+        });
+      }
     });
-    // console.log(`LENTGH---------------- ${errmsg.length}`)
-    if (errmsg.length > 0) {
-      res.json(errmsg);
-    } else {
-      db.User.create(userSubmission).then(function(dbUserData) {
-        req.session.username = userSubmission.name;
-        res.json("/games");
-      }).catch(function(error) {
-        console.log("Inside of catch from userinfo POST: \n", error);
-      });
-    }
   });
   app.post("/verifyUser", function(req, res) { //index calls this on form submission
     let userSubmission = {
@@ -77,36 +71,64 @@ module.exports = function(app, express) {
     });
   });
   // Route for rendering the games page for the client
-  app.get("/games", async function(req, res) {
+  app.get("/games", function(req, res) {
     console.log("/games called");
     if (req.session.username === undefined) res.redirect("/");
     //below fetch HIGHSCORES
     let flappy_bird_score, snake_score, ping_pong_score;
-    let user_table;
-    await db.User.findAll({}).then(function(dbRaw) {
-      user_table = JSON.parse(JSON.stringify(dbRaw));
-    });
-    await db.Score.findAll({ where: { name: "snake" } }).then(function(dbRaw) {
-      snake_score = JSON.parse(JSON.stringify(dbRaw)).sort((a, b) => b.score - a.score).map(row => {
-        return { username: user_table[row.UserId - 1].name, score: row.score };
-      }).slice(0, 5);
-    });
-    await db.Score.findAll({ where: { name: "flappy_bird" } }).then(function(dbRaw) {
-      flappy_bird_score = JSON.parse(JSON.stringify(dbRaw)).sort((a, b) => b.score - a.score).map(row => {
-        return { username: user_table[row.UserId - 1].name, score: row.score };
-      }).slice(0, 5);
-    });
-    await db.Score.findAll({ where: { name: "ping_pong" } }).then(function(dbRaw) {
-      ping_pong_score = JSON.parse(JSON.stringify(dbRaw)).sort((a, b) => b.score - a.score).map(row => {
-        return { username: user_table[row.UserId - 1].name, score: row.score };
-      }).slice(0, 5);
-    });
-    res.render("game", {
-      username: req.session.username,
-      flappy_bird_score: flappy_bird_score,
-      snake_score: snake_score,
-      ping_pong_score: ping_pong_score
-    });
+    db.User.findAll({}).then(function(dbRaw) {
+      let user_table = JSON.parse(JSON.stringify(dbRaw));
+      return user_table;
+    }).then((user_table) => { //find snake
+      db.Score.findAll({
+        where: { name: "snake" },
+        order: [
+          ['score', 'DESC'],
+          ['createdAt', 'DESC']
+        ]
+      }).then(function(dbRaw) {
+        snake_score = JSON.parse(JSON.stringify(dbRaw)).map(row => {
+          return { username: user_table[row.UserId - 1].name, score: row.score };
+        }).slice(0, 5);
+        console.log("snake");
+      }).then(() => { //then flappy
+        db.Score.findAll({
+          where: { name: "flappy_bird" },
+          order: [
+            ['score', 'DESC'],
+            ['createdAt', 'DESC']
+          ]
+        }).then(function(dbRaw) {
+          flappy_bird_score = JSON.parse(JSON.stringify(dbRaw)).map(row => {
+            return { username: user_table[row.UserId - 1].name, score: row.score };
+          }).slice(0, 5);
+          console.log("flap");
+        }).then(() => { //then pong
+          db.Score.findAll({
+            where: { name: "ping_pong" },
+            order: [
+              ['score', 'DESC'],
+              ['createdAt', 'DESC']
+            ]
+          }).then(function(dbRaw) {
+            ping_pong_score = JSON.parse(JSON.stringify(dbRaw)).map(row => {
+              return { username: user_table[row.UserId - 1].name, score: row.score };
+            }).slice(0, 5);
+            console.log("pong");
+          }).then(() => { //then return
+            console.log("return");
+            const renderObj = {
+              username: req.session.username,
+              flappy_bird_score: flappy_bird_score,
+              snake_score: snake_score,
+              ping_pong_score: ping_pong_score
+            };
+            console.log("post-then", renderObj);
+            res.render("game", renderObj);
+          })
+        })
+      })
+    })
   });
   // Route for rendering the play Snake page for the client
   app.get("/playSnake", function(req, res) {
